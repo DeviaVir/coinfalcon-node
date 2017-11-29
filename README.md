401e20c43b1a0639658b84ac1861e096845cfef5
Chase Sillevis <chase@sillevis.net>
2017-11-29 13:47:44 +0100

    Works now.

--- README.md
+++ README.md
@@ -3,10 +3,6 @@ Node.js module for Coinfalcon.com API
 
 [![Build Status](https://secure.travis-ci.org/DeviaVir/coinfalcon-node.png?branch=master)](https://travis-ci.org/DeviaVir/coinfalcon-node.svg?branch=master)
 
-# Warning: this is currently a work in progress and not working yet.
-
-Follow https://github.com/carlos8f/zenbot/issues/756 for up-to-date status.
-
 # Coinfalcon
 
 With this Node module you can plug into the no-fees and fast [Coinfalcon](https://coinfalcon.com/).
--- src/client/index.js
+++ src/client/index.js
@@ -30,7 +30,7 @@ export default class Client {
 
   signature (...args) {
     const hmac = crypto.createHmac('sha256', this.secret);
-    hmac.update(`${args[0]}|/api/${this.version}/${args[1]}|` + (args[2] ? `${JSON.stringify(args[2])}` : ''));
+    hmac.update(`${args[0]}|/api/${this.version}/${args[1]}|` + (args[2] ? `${JSON.stringify(args[2])}` : '{}'));
     return hmac.digest('hex');
   }
 
@@ -38,16 +38,10 @@ export default class Client {
     const method = String(args[0]).toUpperCase();
     const requestPath = (Client.requestHasBody(method) ? args.slice(1, -1) : args.slice(1)).join('/');
     const body = (Client.requestHasBody(method) ? args.slice(-1)[0] : null);
-    const signature = this.signature(method, requestPath, body);
-    let timestamp = Date.now().toString();
-    timestamp = timestamp.substring(0, timestamp.length - 3);
+
     const req = {
       'hostname': 'coinfalcon.com',
       'headers': {
-        'CF-API-KEY': this.key,
-        'CF-API-SECRET': this.secret,
-        'CF-API-TIMESTAMP': parseInt(timestamp, 10),
-        'CF-API-SIGNATURE': signature,
         'Content-Type': 'application/json; charset=utf-8'
       },
       'path': '/api/' + this.version + '/' + requestPath,
@@ -55,6 +49,19 @@ export default class Client {
       'method': method,
       'body': body
     };
+
+    if (this.version === 'v1' && requestPath.indexOf('markets/') === 0) {
+      // public request
+    } else {
+      const signature = this.signature(method, requestPath, body);
+      let timestamp = Date.now().toString();
+      timestamp = timestamp.substring(0, timestamp.length - 3);
+
+      req.headers['CF-API-KEY'] = this.key;
+      req.headers['CF-API-SECRET'] = this.secret;
+      req.headers['CF-API-TIMESTAMP'] = parseInt(timestamp, 10);
+      req.headers['CF-API-SIGNATURE'] = signature;
+    }
 
     return https.request(req);
   }
