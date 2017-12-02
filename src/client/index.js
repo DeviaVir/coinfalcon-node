03628e06cc12bff4948099db845ed4e5362842e8
seaify <dilin.life@gmail.com>
2017-12-02 21:20:24 +0800

    update signature way

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
 import crypto from 'crypto';
-import https from '../libs/https';
+var http_request = require('request');
 
 export default class Client {
   constructor (key, secret, version = 'v1') {
@@ -30,40 +30,70 @@ export default class Client {
 
   signature (...args) {
     const hmac = crypto.createHmac('sha256', this.secret);
-    hmac.update(`${args[0]}|/api/${this.version}/${args[1]}|` + (args[2] ? `${JSON.stringify(args[2])}` : '{}'));
+    console.log('payload')
+    if (args[1] == 'GET') {
+      var payload = `${args[0]}|${args[1]}|/api/${this.version}/${args[2]}`
+      hmac.update(`${args[0]}|${args[1]}|/api/${this.version}/${args[2]}`);
+    }
+    else {
+      var payload = `${args[0]}|${args[1]}|/api/${this.version}/${args[2]}|${JSON.stringify(args[3])}`
+      hmac.update(`${args[0]}|${args[1]}|/api/${this.version}/${args[2]}|${JSON.stringify(args[3])}`);
+    }
+    console.log(payload)
     return hmac.digest('hex');
   }
 
+  serialize (obj) {
+    var str = [];
+    for(var p in obj)
+      if (obj.hasOwnProperty(p)) {
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+      }
+    return str.join("&");
+  }
+
   request (...args) {
+    console.log(args[0]);
+    console.log([args[1], this.serialize(args[2])].join('/'));
     const method = String(args[0]).toUpperCase();
-    const requestPath = (Client.requestHasBody(method) ? args.slice(1, -1) : args.slice(1)).join('/');
-    const body = (Client.requestHasBody(method) ? args.slice(-1)[0] : null);
+    const requestPath = (Client.requestHasBody(method) ?  args[1]: [args[1], this.serialize(args[2])].join('?'));
+    const body = (Client.requestHasBody(method) ? args.slice(-2)[0] : null);
+
+    console.log('requestpath is ');
+    console.log(requestPath);
 
     const req = {
-      'hostname': 'coinfalcon.com',
       'headers': {
         'Content-Type': 'application/json; charset=utf-8'
       },
-      'path': '/api/' + this.version + '/' + requestPath,
-      'port': 443,
+      'url': 'http://localhost:3000/api/' + this.version + '/' + requestPath,
       'method': method,
+      // 'body': body
+      'json': true,
       'body': body
     };
 
     if (this.version === 'v1' && requestPath.indexOf('markets/') === 0) {
       // public request
     } else {
-      const signature = this.signature(method, requestPath, body);
       let timestamp = Date.now().toString();
       timestamp = timestamp.substring(0, timestamp.length - 3);
+      const signature = this.signature(timestamp, method, requestPath, body);
+
 
       req.headers['CF-API-KEY'] = this.key;
       req.headers['CF-API-SECRET'] = this.secret;
       req.headers['CF-API-TIMESTAMP'] = parseInt(timestamp, 10);
       req.headers['CF-API-SIGNATURE'] = signature;
     }
-
-    return https.request(req);
+    var callback = args[args.length - 1];
+    console.log(req);
+    return http_request(req, function (err, res, body) {
+      // Connection header by default is keep-alive,
+      // we have to manualle end the socket
+      callback(err, res, body);
+    });
+    //return http_request(req);
   }
 
   static requestHasBody (method) {
