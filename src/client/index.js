a25703339407e9e62787f5ed571d945d1d84dedf
Chase <chase@sillevis.net>
2017-12-27 11:19:44 +0100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -32,7 +32,7 @@ export default class Client {
     let body = {};
     if (bodyConst) body = bodyConst;
     const hmac = crypto.createHmac('sha256', this.secret);
-    hmac.update(`${timestamp}|${method}|/api/${this.version}/${requestPath}` + (method != 'GET' ? `|${JSON.stringify(body)}` : ''));
+    hmac.update(`${timestamp}|${method}|/api/${this.version}/${requestPath}` + (method !== 'GET' ? `|${JSON.stringify(body)}` : ''));
     return hmac.digest('hex');
   }
 
